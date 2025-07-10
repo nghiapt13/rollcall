aa3e4941f7bfe32ed42418bdd178da4135e3ff9d
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2025-07-10 09:11:24 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,32 +2,30 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import { isAdminUser } from '@/config/authorized-users';
 import { clerkClient } from '@clerk/nextjs/server';
+import {get} from '@vercel/edge-config'
 
 
 
-
-// Định nghĩa các route cần bảo vệ (chỉ admin mới truy cập được)
 const isProtectedRoute = createRouteMatcher([
   '/admin(.*)',
-  // Thêm các route khác cần bảo vệ ở đây
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
-  // if (!process.env.EDGE_CONFIG) {
-  //   req.nextUrl.pathname = `/missing-edge-config`
-  //   return NextResponse.rewrite(req.nextUrl)
-  // }
+  if (!process.env.EDGE_CONFIG) {
+    req.nextUrl.pathname = `/missing-edge-config`
+    return NextResponse.rewrite(req.nextUrl)
+  }
 
-  // const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
+  const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
 
-  // if (isInMaintenanceMode) {
-  //   req.nextUrl.pathname = `/maintenance`
+  if (isInMaintenanceMode) {
+    req.nextUrl.pathname = `/maintenance`
 
-  //   // Rewrite to the url
-  //   return NextResponse.rewrite(req.nextUrl)
-  // }
+    // Rewrite to the url
+    return NextResponse.rewrite(req.nextUrl)
+  }
 
   // Nếu route được bảo vệ
   if (isProtectedRoute(req)) {
