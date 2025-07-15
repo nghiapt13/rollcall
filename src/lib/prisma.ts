eadafb57d858f5ddda35d88577dd72b84e7e546f
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2025-07-15 19:08:24 +0700

    add

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient } from 'app/generated/prisma/client'
+import { PrismaClient } from '@/generated/prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
