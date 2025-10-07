1af822e35c96b43b42c5d75428110c486a1a1a99
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2025-10-07 22:09:48 +0700

    update

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -9,17 +9,17 @@ export const metadata: Metadata = {
 
 export default function MaintenancePage() {
     return (
-        <div className="bg-gray-100">
+        <div className="">
             <div className="min-h-screen flex flex-col justify-center items-center">
                 <Image
-                    src="/2020-Btec.png"
+                    src="/Btec-knight.png"
                     alt="BTEC"
                     width={350}
                     height={350}
-                    className="mb-5"
+                    className="mb-4"
                 />
-                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 mb-4">TRANG WEB ĐANG ĐƯỢC BẢO TRÌ</h1>
-                <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">Anh/chị vui lòng thử lại sau</p>
+                <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-black mb-4">404 NOT FOUND</h1>
+                {/* <p className="text-center text-gray-500 text-lg md:text-xl lg:text-2xl mb-8">Anh/chị vui lòng thử lại sau</p> */}
             </div>
         </div>
     );
