41bf5577dcbd85cca092c78aa067199a7dc6c2da
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2025-10-07 22:17:04 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,20 @@ const isProtectedRoute = createRouteMatcher([
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
+    if (!process.env.EDGE_CONFIG) {
+    req.nextUrl.pathname = `/missing-edge-config`
+    return NextResponse.rewrite(req.nextUrl)
+  }
+
+  const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
+
+  if (isInMaintenanceMode) {
+    req.nextUrl.pathname = `/maintenance`
+
+    // Rewrite to the url
+    return NextResponse.rewrite(req.nextUrl)
+  }
+
   // Chỉ kiểm tra đăng nhập, không kiểm tra role ở đây
   if (isProtectedRoute(req) && !userId) {
     return NextResponse.redirect(new URL('/sign-in', req.url));
